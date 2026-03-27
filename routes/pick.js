396530c87cc52835d86c91ce9db85c067dde92d1
Poornima kallam <145594732+s559222@users.noreply.github.com>
2026-03-27 14:41:37 -0500

    random files

--- routes/pick.js
+++ routes/pick.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('randomitem', { title: 'A random item' });
+router.get("/", function (req, res, next) {
+  res.render("randomitem", { title: "Random Images" });
 });
 
 module.exports = router;
