5d081d11601ec9c008fa5370eb802054b98e79ea
Poornima kallam <145594732+s559222@users.noreply.github.com>
2026-03-27 14:15:46 -0500

    query strings

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var landmarksRouter = require("./routes/landmarks");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var gridRouter = require("./routes/grid");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/landmarks", landmarksRouter);
+app.use("/grid", gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
